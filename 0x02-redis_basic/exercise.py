3b60f86be09e654293004eca47427b6969d930d9
Benard Kiplangat <benardkiplangat97@gmail.com>
2024-08-14 21:42:50 +0300

    Reading from Redis and recovering original type because redis store data as string, bytes, and numbers

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from typing import Union
+from typing import Union, Callable, Optional
 from uuid import uuid4
 import redis
 
@@ -11,19 +11,47 @@ import redis
 
 class Cache:
     '''
-        Cache class.
+        The Cache class.
     '''
     def __init__(self):
         '''
-            Initialize the cache.
+            Initializing the cache class.
         '''
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''
-            Store data in the cache.
+            Storing data in the cache.
         '''
         randomKey = str(uuid4())
         self._redis.set(randomKey, data)
         return randomKey
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        '''
+            Getting data from the cache.
+        '''
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        '''
+            Getting a string from the cache.
+        '''
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        '''
+            Getting an int from the cache.
+        '''
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
